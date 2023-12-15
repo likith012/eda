6ffe6c09d2b71f7023c778a64188345a4df557ca
likith012 <likith012@gmail.com>
2023-12-15 21:04:07 +0530

    Add KafkaModel.java and update KafkaModelTest.java

--- kafka/kafka-model/src/test/java/com/microservices/kafka/kafkamodel/KafkaModelTest.java
+++ kafka/kafka-model/src/test/java/com/microservices/kafka/kafkamodel/KafkaModelTest.java
@@ -1,11 +1,10 @@
 package com.microservices.kafka.kafkamodel;
 
-import com.microservices.kafka.kafkamodel.KafkaModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class KafkaModelTest {
+class KafkaModelTest {
     private KafkaModel kafkaModel;
 
     @BeforeEach
@@ -14,28 +13,28 @@ public class KafkaModelTest {
     }
 
     @Test
-    public void testUserId() {
+    void testUserId() {
         long userId = 123L;
         kafkaModel.setUserId(userId);
         assertEquals(userId, kafkaModel.getUserId());
     }
 
     @Test
-    public void testId() {
+    void testId() {
         long id = 456L;
         kafkaModel.setId(id);
         assertEquals(id, kafkaModel.getId());
     }
 
     @Test
-    public void testText() {
+    void testText() {
         String text = "Test text";
         kafkaModel.setText(text);
         assertEquals(text, kafkaModel.getText());
     }
 
     @Test
-    public void testCreatedAt() {
+    void testCreatedAt() {
         long createdAt = System.currentTimeMillis();
         kafkaModel.setCreatedAt(createdAt);
         assertEquals(createdAt, kafkaModel.getCreatedAt());
